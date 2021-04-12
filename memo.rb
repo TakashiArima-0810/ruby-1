89e306d3d287e098065afe416cdd9d03352c9cf1
有馬高志 <arimatakashi@arimatakashinoMacBook-Pro.local>
2021-04-12 09:28:24 +0900

    訂正

--- memo.rb
+++ memo.rb
@@ -1,33 +1,34 @@
 require "csv"
 
 
-puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+puts "1(新規でメモを作成) 2(既存のメモ追記する)"
 puts "いずれかのを入力し、Enterキーを押してください"
 
 memo_type = gets.to_i
+
 if memo_type == 1
-  puts "作成したいファイル名(拡張子除く)を入力してください"
-  file_name = gets
+  puts "作成したいファイル名(拡張子除く)を入力し、完了したらEnterキーを押してください"
+  file_name = gets.chomp
   
-  puts "メモしたい内容を入力し、完了したらEnterキーを押してください"
-  memo = gets.chomp
+  puts "メモしたい内容を入力し、完了したらCtrl + Dキーを押してください"
+  memo = readlines
   
   CSV.open("#{file_name}.csv","w") do |csv|
     csv << ["#{memo}"]
   end
   
-  elsif memo_type == 2
-    puts "追記したいファイル名(拡張子除く)を入力してください"
-    puts "ファイル名が見つからない場合、新規に作成します"
-    file_name = gets
+elsif memo_type == 2
+  puts "追記したいファイル名(拡張子除く)を入力し、Ctrl + Dキーを押してください"
+  puts "ファイル名が見つからない場合、新規に作成します"
+  file_name = gets.chomp
   
-    puts "追記したい内容を入力し、完了したらEnterキーを押してください"
-    memo = gets.chomp
+  puts "追記したい内容を入力し、完了したらCtrl + Dキーを押してください"
+  memo = readlines
   
-    CSV.open("#{file_name}.csv","a+") do |csv|
+  CSV.open("#{file_name}.csv","a+") do |csv|
     csv << ["#{memo}"]
-    end
-  
-  else
-    puts "入力が誤っています。再度最初から実行して下さい"
   end
+  
+else
+  puts "入力が誤っています。再度最初から実行して下さい"
+end
